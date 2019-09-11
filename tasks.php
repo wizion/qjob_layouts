9fe34ade096b445afd5854ec5ec6aa6961bf2971
Wizion <wizion94@gmail.com>
2019-09-11 10:25:52 +0300

    trash

--- tasks.php
+++ tasks.php
@@ -1,7 +1,60 @@
 <?php
 include 'header.php';
 ?>
+<section class="taskPage">
+    <div class="titleAndMapButton">
+        <div>
+            <div class="mapButton on"></div>
+        </div>
+    </div>
+    <div class="mapBlock show">
 
+    </div>
+    <div class="advList">
+        <div class="item">
+            <div class="avatar">
+                <div class="avatarBlock">
+                    <img src="" alt="">
+                </div>
+            </div>
+            <div class="allDesc">
+                <div class="status">
+
+                </div>
+                <h4 class="title"></h4>
+                <div class="address"></div>
+                <div class="date"><span></span></div>
+                <div class="riskDeal">
+
+                </div>
+                <div class="price">
+                    <span></span>
+                </div>
+                <div class="authorName"></div>
+                <div class="likes">
+                    <span class="like"></span>
+                    <span class="dislike"></span>
+                </div>
+                <div class="buttons">
+                    <div class="favorite"></div>
+                    <div class="dontshow"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="sidebarFilter">
+        <div class="searchFieldMobile">
+            <div class="filterBlock">
+                <div class="cityField"></div>
+                <div class="radiusOfSearch">
+
+                </div>
+                <div class="priceValue"></div>
+                <div class="searchField"></div>
+            </div>
+        </div>
+    </div>
+</section>
 <?php
 include 'footer.php';
 ?>
